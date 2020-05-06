0bbbbe26cf9952cb9ea533c74762e1d1e3da9211
showerlee <showerlee@vip.qq.com>
2020-05-06 18:08:51 +0800

    Add region specific to each env

--- src/awsweeper.py
+++ src/awsweeper.py
@@ -6,11 +6,13 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+    region = os.environ['AWS_REGION']
+
     CMD1=["cp", "-a", "bin", "config", "/tmp"]
     subprocess.run(CMD1)
 
     os.chdir("/tmp")
 
-    CMD2=["./bin/awsweeper", "--region", "ap-southeast-1", "--dry-run", "./config/aws_security_group.yml"]
+    CMD2=["./bin/awsweeper", "--region", region, "--dry-run", "./config/aws_security_group.yml"]
     output2 = subprocess.check_output(CMD2, universal_newlines=True)
     logger.info(output2)
