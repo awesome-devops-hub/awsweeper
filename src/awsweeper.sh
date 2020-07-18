c46c6cf5fb65f250a5de134ebf30415510b50a28
showerlee <showerlee@vip.qq.com>
2020-07-18 15:09:24 +0800

    Let instance terminate itself after cleanup

--- src/awsweeper.sh
+++ src/awsweeper.sh
@@ -3,7 +3,8 @@
 cd $(dirname $0)/..
 
 APP_NAME="awsweeper"
-TIME_STAMP="$(TZ=Singapore date +'%Y%m%d%H%M')"
+TIME_STAMP="$(TZ=Asia/Shanghai date +'%Y%m%d%H%M')"
+instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 SWEEP_REGION=("ap-northeast-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "us-east-1" "us-east-2")
 AWSWEEPER_LOG="/tmp/awsweeper-${TIME_STAMP}.log"
 AWSWEEPER_CFG="./config/aws_resource.yml"
@@ -20,5 +21,7 @@ do
 done
 
 echo ">>> Sync ${AWSWEEPER_LOG} to S3 bucket ${ARTEFACT_BUCKET}-${AWS_DEFAULT_REGION} <<<" 2>&1 | tee -a ${AWSWEEPER_LOG}
-aws s3 cp "${AWSWEEPER_LOG}" "s3://${ARTEFACT_BUCKET}-${AWS_DEFAULT_REGION}/log/"
 echo ">>> Done <<<" 2>&1 | tee -a ${AWSWEEPER_LOG}
+aws s3 cp "${AWSWEEPER_LOG}" "s3://${ARTEFACT_BUCKET}-${AWS_DEFAULT_REGION}/log/"
+
+aws autoscaling terminate-instance-in-auto-scaling-group --instance-id "$instance_id" --should-decrement-desired-capacity --region "$AWS_DEFAULT_REGION" | tr '\n' ' ' | xargs -0 echo
